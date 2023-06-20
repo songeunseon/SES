6e730168601eb93c53c6aae92298ac06a21136f1
songeunseon <ses920525@gmail.com>
2023-06-20 17:32:33 +0900

    2023-06-20

---
+++ 2023-06-20/static/js/array5.js
@@ -0,0 +1,55 @@
+let num=new Array();
+let num2=new Array();
+function init(){ //초기화
+    //중복없이 랜덤값 넣기
+    //indexOf를 이용해서 -1이 나오면 일치하는게 없다.
+    //즉 중복되는 숫자가 없다는 뜻이니까 배열에 저장
+    num.push(Math.floor(Math.random()*8));
+    for(var i=1; i<=3; i++){
+        var temp=Math.floor(Math.random()*8);
+        if(num.indexOf(temp)==-1){
+            num.push(temp);
+        }else{
+            i--;
+        }
+    }
+}
+function go(){
+    num2.push(Math.floor(Math.random()*8));
+    for(var i=1; i<=7; i++){
+        var temp=Math.floor(Math.random()*8);
+        if(num2.indexOf(temp)==-1){
+            num2.push(temp);
+        }else{
+            i--;
+        }
+    }
+}
+function start(){
+    /*let pic1=document.getElementById("pic1");
+    let pic2=document.getElementById("pic2");
+    let pic3=document.getElementById("pic3");
+    let pic4=document.getElementById("pic4");
+    let pic5=document.getElementById("pic5");
+    let pic6=document.getElementById("pic6");
+    let pic7=document.getElementById("pic7");
+    let pic8=document.getElementById("pic8");
+
+    pic1.addEventListener("click", same_search);
+    pic2.addEventListener("click", same_search);
+    pic3.addEventListener("click", same_search);
+    pic4.addEventListener("click", same_search);
+    pic5.addEventListener("click", same_search);
+    pic6.addEventListener("click", same_search);
+    pic7.addEventListener("click", same_search);
+    pic8.addEventListener("click", same_search);*/
+    
+    //class로 가져오게되면 배열로 저장되어진다
+    init(); //초기화 함수 실행
+    go();
+    let pic=document.getElementsByClassName("picture");
+    for(var i=0; i<pic.length; i++){
+        //pic[i].addEventListener("click", same_search);
+        pic[num2[i]].innerHTML=num[i%4];
+    }
+}
