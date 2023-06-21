1008c41632dd67bc56071a835ddca5e987715261
songeunseon <ses920525@gmail.com>
2023-06-21 09:48:06 +0900

    2023-06-21

--- 2023-06-20/static/js/array5.js
+++ 2023-06-20/static/js/array5.js
@@ -1,20 +1,18 @@
-let num=new Array();
-let num2=new Array();
+let num=new Array();//화면에 표시되는 숫자 저장배열
+let num2=new Array();//숫자가 출력될 위치 저장 배열
 function init(){ //초기화
     //중복없이 랜덤값 넣기
     //indexOf를 이용해서 -1이 나오면 일치하는게 없다.
     //즉 중복되는 숫자가 없다는 뜻이니까 배열에 저장
-    num.push(Math.floor(Math.random()*8));
+    num.push(Math.floor(Math.random()*10)+1);
     for(var i=1; i<=3; i++){
-        var temp=Math.floor(Math.random()*8);
+        var temp=Math.floor(Math.random()*10)+1;
         if(num.indexOf(temp)==-1){
             num.push(temp);
         }else{
             i--;
         }
     }
-}
-function go(){
     num2.push(Math.floor(Math.random()*8));
     for(var i=1; i<=7; i++){
         var temp=Math.floor(Math.random()*8);
@@ -25,7 +23,10 @@ function go(){
         }
     }
 }
-function start(){
+
+
+
+window.onload=function(){
     /*let pic1=document.getElementById("pic1");
     let pic2=document.getElementById("pic2");
     let pic3=document.getElementById("pic3");
@@ -46,10 +47,28 @@ function start(){
     
     //class로 가져오게되면 배열로 저장되어진다
     init(); //초기화 함수 실행
-    go();
+    let start=document.getElementById("start");
+    start.addEventListener("click",play);
+
     let pic=document.getElementsByClassName("picture");
     for(var i=0; i<pic.length; i++){
-        //pic[i].addEventListener("click", same_search);
-        pic[num2[i]].innerHTML=num[i%4];
+        pic[i].addEventListener("click", same_search);
     }
+}
+function play(){
+    let pic = document.getElementsByClassName("back");
+    for( var i=0; i<pic.length; i++){
+        pic[num2[i]].innerHTML = num[i%4] ;
+    }
+    setTimeout(function(){
+        let pic = document.getElementsByClassName("back");
+        for(var i=0; i<pic.length; i++){
+            pic[i].style.display="none";
+        }
+    },2000);//(함수,시간);
+
+}
+
+function  same_search(){
+    
 }
