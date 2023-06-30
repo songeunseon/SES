6e1e86b6e3b5c8eadd32394635688250f65198e3
songeunseon <ses920525@gmail.com>
2023-06-30 17:20:45 +0900

    2023-06-30

---
+++ 2023-06-30/static/js/worldcup.js
@@ -0,0 +1,13 @@
+
+window.onload=function(){
+    let start=document.getElementById("start");
+    start.addEventListener("click", function(){
+        alert("나는 바보다");
+    });
+}
+function pick1() {
+    alert("요시키");
+}
+function pick2() {
+    alert("이시키"); 
+}
