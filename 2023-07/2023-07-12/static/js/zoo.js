f6f93a94b40620cae9f41061f50eaf3222b0aaf5
songeunseon <ses920525@gmail.com>
2023-07-14 10:27:33 +0900

    2023-07-14

--- 2023-07/2023-07-12/static/js/zoo.js
+++ 2023-07/2023-07-12/static/js/zoo.js
@@ -1,6 +1,27 @@
 
 
 window.onload=function(){
+    var pre=document.getElementsByClassName("prev")[0];
+    var next=document.getElementsByClassName("next")[0];
+    pre.addEventListener("click",function(){
+        var slideview=document.getElementsByClassName("eventslide")[0];
+        slideview.style.transform="translate3d(0px, 0px, 0px) ";
+        // var slideview=document.getElementsByClassName("eventslide")[0]; //ul태그
+        // var now_left=slideview.style.left.split("px")[0]; //현재 left값 가져오기
+        // if(now_left == 0) return; //첫번째 li가 화면에 보이면 이동안되게
+        // var left=Number(now_left)+312; //li 하나당 312px 이기 때문에
+        // slideview.style.left=left+"px";
+    });
+    next.addEventListener("click",function(){
+        var slideview=document.getElementsByClassName("eventslide")[0];
+        slideview.style.transform="translate3d(-936px, 0px, 0px) ";
+        // var slideview=document.getElementsByClassName("eventslide")[0]; //ul태그
+        // var now_left=slideview.style.left.split("px")[0]; //현재 left값 가져오기
+        // if(now_left == -936) return; //마지막 li가 화면에 보이면 이동안되게
+        // var left=now_left-312; //li 하나당 312px 이기 때문에
+        // slideview.style.left=left+"px";
+    });
+
     var srhBt = document.getElementsByClassName("search_button");
     srhBt[0].addEventListener("click",function(){
         var bar=document.querySelectorAll(".search_bar")[0];
