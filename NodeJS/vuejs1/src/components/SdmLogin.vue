31f9b6edabb1fd327e1349ca61e7ae5d54d80f3d
songeunseon <ses920525@gmail.com>
2023-09-04 17:16:46 +0900

    2023-09-04

---
+++ NodeJS/vuejs1/src/components/SdmLogin.vue
@@ -0,0 +1,114 @@
+<template>
+    <main id="main">
+        <section id="content">
+            <h3>SDM garden 로그인</h3>
+            <div class="login_wrap">
+                <form name="loginf" id="loginf" method="get" action="/">
+                    <input type="hidden" name="part" value="login_check">
+                    <div class="login_box">
+                        <div class="login_input">
+                            <input type="text" name="sdmId" id="sdmId" placeholder="abc123@naver.com">
+                            <label>ID</label>
+                        </div>
+                        <div class="login_input">
+                            <input type="password" name="sdmPw" id="sdmPw">
+                            <label>PW</label>
+                        </div>
+                        <div class="login_bt">
+                            <a href="javascript:login();">LOGIN</a>
+                        </div>
+                    </div>
+                </form>
+                <div class="joinAfind">
+                    <p class="find">아이디 / 비밀번호 찾기</p>
+                    <p class="join"><a href="/sign">회원가입</a></p>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+<script>
+    export default {
+        name:'SdmLogin',
+    }
+</script>
+<style>
+#main{
+    width: 700px;
+    margin: 0 auto;
+}
+#content{
+    width: 100%;
+    margin: 50px;
+}
+#content h3{
+    text-align: center;
+    font-size: 30px;
+    color: pink;
+    padding-bottom: 10px;
+    border-bottom: 3px solid pink;
+}
+.login_box{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    row-gap: 20px;
+    position: relative;
+    padding: 50px;
+}
+.login_input label{
+    font-size: 20px;
+    padding: 0 10px ;
+    font-weight: 900;
+}
+.login_input input{
+    width: 250px;
+    height: 30px;
+    text-align: center;
+    font-size: 20px;
+    outline: none;
+    border: 0;
+    border-bottom: 2px solid skyblue;
+}
+.login_bt{
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    font-size: 20px;
+    text-align: center;
+    padding: 60px 40px;
+}
+.login_bt a{
+    color: white;
+    text-decoration: none;
+    padding: 40px 10px;
+    background: skyblue;
+    cursor: pointer;
+}
+.login_bt a:hover{
+    color: skyblue;
+    background: white;
+    border: 3px solid skyblue;
+}
+.joinAfind{
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    font-size: 20px;
+    padding-top: 50px;
+    color: #aaa;
+}
+
+.join a{
+    color: pink;
+    font-weight: 900;
+    text-decoration: none;
+    padding: 10px 5px;
+    cursor: pointer;
+}
+.join a:hover{
+    border: 3px solid pink;
+    border-left: 0;
+    border-right: 0;
+}
+</style>
---
+++ NodeJS/vuejs1/src/components/SdmSign.vue
@@ -0,0 +1,90 @@
+<template>
+    <main id="main">
+        <section id="content">
+            <h3>SDM garden 회원가입</h3>
+            <div id="jId"><label>아이디</label><input type="email" name="sdmId" id="sdmId" placeholder="ex-abc123@naver.com"></div>
+            <div id="jPw"><label>비밀번호</label><input type="password" name="sdmPw" id="sdmPw" placeholder="특수기호포함 8~16자리"></div>
+            <div id="jTel"><label>연락처</label><input type="tel" name="tel" id="tel" placeholder="'-'하이픈 제외"></div>
+            <div id="jAddr"><label>신혼집주소</label><input type="text" name="addr" id="addr" placeholder="신주소로 입력"></div>
+            <div id="jDate"><label>결혼예정일</label><input type="date" name="wDate" id="wDate"></div>
+        </section>
+        <div id="join">
+            <button id="jbt">♡ 작 성 완 료 ♡</button>
+        </div>
+    </main>
+</template>
+<script>
+    export default {
+        name:'SdmSign',
+    }
+</script>
+<style>
+#main{
+    width: 700px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+}
+#content h3{
+    border-bottom: 3px solid pink;
+    padding-bottom: 20px;
+    font-size: 30px;
+    text-align: center;
+}
+#content{
+    width: 700px;
+    padding: 20px;
+    color: pink;
+    background: white;
+    border: 3px dashed pink;
+}
+#content div{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding: 0 100px;
+
+}
+#content div input{
+    width: 300px;
+    margin: 20px;
+    outline: none;
+    border: 0;
+    border-bottom: 3px double pink;
+    text-align: center;
+    font-size: 15px;
+}
+#sdmId, #sdmPw, #tel, #addr, #wDate{
+    color: #aaa;
+
+}
+#content div label{
+    margin: 20px 0;
+    font-size: 20px;
+    color: black;
+}
+#jbt{
+    width: 100%;
+    margin-top: 10px;
+    background: skyblue;
+    border: 0;
+    font-size: 20px;
+    color: white;
+    opacity: 0.3;
+    cursor: pointer;
+    padding: 10px 0;
+}
+#jbt:hover{
+    opacity: 1;
+    background: white;
+    color: skyblue;
+    border: 3px solid skyblue;
+    border-left: 0;
+    border-right: 0;
+
+}
+#wDate{
+    font-weight: 800;
+}
+
+</style>
